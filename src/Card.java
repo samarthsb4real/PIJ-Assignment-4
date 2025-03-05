f6a3aa6fbebc7d262be7ec8b0f8135dc5ebd4869
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-05 19:25:03 +0530

    Add Card class with suit and rank properties

---
+++ src/Card.java
@@ -0,0 +1,16 @@
+/**
+ * Name: Samarth Sandesh Bhadane
+ * PRN: 123456789
+ * Batch: A
+ */
+
+ public class Card {
+    private String suit;
+    private String rank;
+
+    // Constructor to initialize card properties
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+}
