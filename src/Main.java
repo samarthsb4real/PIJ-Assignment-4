75a83c35c883f7a166da65fa45d8081553b93ec7
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-06 23:47:09 +0530

    Add Main class to initialize the Deck and handle user input

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Name: Samarth Sandesh Bhadane
+ * PRN: 123456789
+ * Batch: A
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Deck deck = new Deck();
+    }
+}
